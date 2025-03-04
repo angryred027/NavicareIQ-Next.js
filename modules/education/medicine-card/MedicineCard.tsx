baf485e679e3e100e70517cde88183f360465a7c
Angry Red <angryred027@gmail.com>
2025-03-04 06:32:33 -0800

    Fix MedicineCard comopent style

--- modules/education/medicine-card/MedicineCard.tsx
+++ modules/education/medicine-card/MedicineCard.tsx
@@ -16,10 +16,10 @@ export default function MedicineCard({ image, name, category, uses }: MedicineCa
             label={category}
             className="bg-[#4167AF] font-inter font-normal text-[0.75rem] leading-[1rem] text-center text-white flex-none order-0 grow-0"
           />
-          <span className="pt-2 font-inter font-semibold text-[0.875rem] leading-[1.25rem] text-[#000005] flex-none order-0 self-stretch grow-0">
+          <span className="block pt-2 font-inter font-semibold text-[0.875rem] leading-[1.25rem] text-[#000005] flex-none order-0 self-stretch grow-0">
             {name}
           </span>
-          <span className="py-2 font-inter font-normal text-[0.75rem] leading-[1rem] text-[#757B80] flex-none order-1 self-stretch grow-0">
+          <span className="block py-2 font-inter font-normal text-[0.75rem] leading-[1rem] text-[#757B80] flex-none order-1 self-stretch grow-0">
             {uses}
           </span>
         </div>
