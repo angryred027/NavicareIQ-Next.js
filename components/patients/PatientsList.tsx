2c1f6614168010e4b6b097b6f1a97d15479b2f00
johndanylak <johndanylak21@gmail.com>
2025-03-05 17:23:34 +0100

    Auto stash before checking out "origin/dev"

--- components/patients/PatientsList.tsx
+++ components/patients/PatientsList.tsx
@@ -12,7 +12,69 @@ import { Badge } from '../common';
 const PatientsList: FC = () => {
   const router = useRouter();
 
-  const rows: TableTData[] = [];
+  const rows: TableTData[] = [
+    {
+      name: {
+        value: "Byron D'Amore",
+        subvalue: (
+          <div className="flex items-center gap-[2px]">
+            Male <Image src={MaleIcon} alt="Male icon" width={16} height={16} />
+          </div>
+        ),
+      },
+      DOB: {
+        value: 'Feb 12, 1995',
+        subvalue: '29 years',
+      },
+      email: 'damore@gmail.com',
+      'Phone number': '(197) 260-7456',
+      address: '1234 Main St, Anytown, USA',
+      'Patient Since': 'Jan 1, 2022',
+      Insurance: 'Aetna',
+      'Reports No.': '3',
+    },
+    {
+      name: {
+        value: 'Jane Doe',
+        subvalue: (
+          <div className="flex items-center gap-[2px]">
+            Female <Image src={FemaleIcon} alt="female icon" width={16} height={16} />
+          </div>
+        ),
+      },
+      DOB: {
+        value: 'Jan 1, 2000',
+        subvalue: '25 years',
+      },
+      email: 'jane.doe@gmail.com',
+      'Phone number': '(123) 456-7890',
+      address: {
+        value: '1234 Main St, Anytown, USA',
+        subvalue: 'Apt. 123',
+      },
+      'Patient Since': {
+        value: 'Jan 1, 2022',
+        subvalue: '3 months',
+      },
+      Insurance: {
+        value: 'Aetna',
+        subvalue: (
+          <div className="flex items-center gap-[6px]">
+            W2113 69935
+            <Image src={InsuranceIcon} alt="Insurance" width={16} height={16} />
+          </div>
+        ),
+      },
+      'Reports No.': {
+        value: (
+          <div className="flex items-center gap-[8px]">
+            6 <Badge label="+3" />
+          </div>
+        ),
+        subvalue: null,
+      },
+    },
+  ];
 
   const handleRowClick = (row: TRows) => {
     router.push(`/patients/${row.id}`);
