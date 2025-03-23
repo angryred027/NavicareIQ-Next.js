184df3ff4267ecbb627f817727e3f49e3e66b0b1
Angry Red <angryred027@gmail.com>
2025-03-23 18:07:47 -0500

    Update the QuantityDropdown component to return selected value.

--- modules/lib/QuantityDropdown.tsx
+++ modules/lib/QuantityDropdown.tsx
@@ -10,23 +10,55 @@ type QuantityOption = {
 
 type QuantityDropdownProps = {
   quantityOptions?: QuantityOption[];
+  onSelect: (value: number) => void;
 };
 
-const quantityDefaultOptions: QuantityOption[] = [
-  { value: <span className="text-[#4167AF]">1</span>, onClick: () => console.log('1 clicked') },
-  { value: <span className="text-[#4167AF]">2</span>, onClick: () => console.log('2 clicked') },
-  { value: <span className="text-[#4167AF]">3</span>, onClick: () => console.log('3 clicked') },
-  { value: <span className="text-[#4167AF]">4</span>, onClick: () => console.log('4 clicked') },
-  { value: <span className="text-[#4167AF]">5</span>, onClick: () => console.log('5 clicked') },
-];
+export default function QuantityDropdown({ quantityOptions, onSelect }: QuantityDropdownProps) {
+  const [selectedValue, setSelectedValue] = useState<number>(1);
+  const quantityDefaultOptions: QuantityOption[] = [
+    {
+      value: <span className="text-[#4167AF]">1</span>,
+      onClick: () => {
+        handleSelect(1);
+      },
+    },
+    {
+      value: <span className="text-[#4167AF]">2</span>,
+      onClick: () => {
+        handleSelect(2);
+      },
+    },
+    {
+      value: <span className="text-[#4167AF]">3</span>,
+      onClick: () => {
+        handleSelect(3);
+      },
+    },
+    {
+      value: <span className="text-[#4167AF]">4</span>,
+      onClick: () => {
+        handleSelect(4);
+      },
+    },
+    {
+      value: <span className="text-[#4167AF]">5</span>,
+      onClick: () => {
+        handleSelect(5);
+      },
+    },
+  ];
 
-export default function QuantityDropdown({ quantityOptions = quantityDefaultOptions }: QuantityDropdownProps) {
-  const [options, setOptions] = useState<QuantityOption[]>(quantityOptions);
+  const handleSelect = (value: number) => {
+    setSelectedValue(value);
+    onSelect(value);
+  };
+
+  const [options, setOptions] = useState<QuantityOption[]>(quantityDefaultOptions);
 
   return (
     <>
       <DropDown
-        btnLabel={<span className="text-[#4167AF]">1</span>}
+        btnLabel={<span className="text-[#4167AF]">{selectedValue}</span>}
         endSlot={<Icon name="arrowDown" size={16} className="font-semibold text-[#4167AF]" />}
         items={options}
         className="p-1 h-[1rem]  font-inter font-normal text-[0.75rem] leading-4 text-[#757B80] border border-none"
