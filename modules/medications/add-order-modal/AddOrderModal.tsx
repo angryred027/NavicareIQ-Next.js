9743d4126becb427e1da1160204c70024ad26a72
Angry Red <angryred027@gmail.com>
2025-03-23 18:09:31 -0500

    Update the AddOrderModal component to get QuantityDropdown's selected value.

--- modules/medications/add-order-modal/AddOrderModal.tsx
+++ modules/medications/add-order-modal/AddOrderModal.tsx
@@ -10,10 +10,21 @@ import QuantityDropdown from '@/modules/lib/QuantityDropdown';
 import { SelectDropDown } from '@/components/common/inputs/select-dropdown';
 import { DropDown } from '@/components/common';
 
-import { Medication } from '@/types/medication.type';
+import { Medication, MedicationOrderItem } from '@/types/medication.type';
 import { Physician } from '@/types/physician.type';
 import { Pharmacy } from '@/types/pharmacy.type';
 import { Patient } from '@/types/patient.type';
+
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { useDispatch, UseDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from '@/store/store';
+import {
+  addLabOrder,
+  addMedicationOrder,
+  setMedicationQuantity,
+  loadStoredOrderData,
+  clearMedicationOrder,
+} from '@/store/features/orderSlice';
 
 type AddOrderModalProps = {
   isOpen: boolean;
@@ -65,12 +76,12 @@ const medicationData = {
   id: 1,
   name: 'Vertisis Custom Compounded Sublingual DHEA',
   use: 'Hold 15 drops under the tongue for 30 seconds and swallow remaining solution twice per day.',
-  form: 'string',
-  qty_per_pkg: 'string',
+  form: 'Capsule',
+  qty_per_pkg: '20',
   price: 10,
   vial: '0.25 mg/mL',
-  storage: 'string',
-  description: 'string',
+  storage: '2-8 c',
+  description: 'Pain',
   created_at: 'string',
   updated_at: 'string',
 };
@@ -104,6 +115,9 @@ export default function AddOrderModal({
   medication = medicationData,
   onClose,
 }: AddOrderModalProps) {
+  const dispatch = useAppDispatch();
+  const { labOrders, medicationOrders } = useSelector((state: RootState) => state.order);
+
   const [copiedText, setCopiedText] = useState<string>('');
   const textRef = useRef<HTMLInputElement>(null);
   const [copied, setCopied] = useState(false);
@@ -136,129 +150,138 @@ export default function AddOrderModal({
   const [selectedPharmacy, setSelectedPharmacy] = useState<string>('');
   const [selectedPatient, setSelectedPatient] = useState<string>('');
   const [pharmacyAddress, setPharmacyAddress] = useState<string>('');
+  const [medicationQuantity, setMedicationQuantity] = useState<number>(1);
 
   const [reason, setReason] = useState<string>('');
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Prepare data for the API call
+    // dispatch(clearMedicationOrder());
+    const newOrder: MedicationOrderItem = {
+      medication: medicationData,
+      quantity: medicationQuantity,
+    };
+
+    dispatch(addMedicationOrder(newOrder));
   };
 
   return (
     <>
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => {
-          onClose();
-        }}
-        title="Add to Order"
-      >
-        <div className="overflow-y-auto [&::-webkit-scrollbar]:none">
-          <div className="m-1">
-            <SelectDropDown
-              label="Select Patient"
-              options={patientData}
-              value={selectedPatient}
-              onChange={(value) => setSelectedPatient(value)}
-            />
-          </div>
-          {physician.first_name && (
-            <Badge
-              label={`Prescribed by: ${physician.first_name}`}
-              className="mx-1 my-2 rounded-md bg-[#E6E9EA] font-inter font-medium text-[12px] leading-[16px] text-[#757B80]"
-            />
-          )}
-          <div className="flex flex-col justify-center mt-6 p-4 h-[52px] rounded-[12px] bg-[#F6F9FA]">
-            <div className="flex justify-between">
-              <span
-                ref={textRef}
-                className="p-1 font-inter font-medium text-sm leading-[20px] text-[#000005] flex-nowrap"
-              >
-                {medication.name}
-              </span>
-              <div>
-                <Icon name="paper1" onClick={copyToClipboard} />
-              </div>
-            </div>
-            <div className="flex">
-              <div className=" font-inter text-xs leading-[16px] text-[#757B80] flex">
-                <span>Vial: &nbsp;</span>
-                <DropDown
-                  btnLabel={<span className="text-[#4167AF]">{medication.vial}</span>}
-                  endSlot={<Icon name="arrowDown" size={16} className="font-semibold text-[#4167AF]" />}
-                  items={vialOptions}
-                  className="p-1 h-[1rem]  font-inter font-normal text-[0.75rem] leading-4 text-[#757B80] border border-none"
-                />
-              </div>
-              <div className="border border-[#E6F0F8] mx-4"></div>
-              <div className=" font-inter text-xs leading-[16px] text-[#757B80] flex">
-                <span>Qty.&nbsp;</span>
-                <QuantityDropdown />
-              </div>
-              <div className="border border-[#E6F0F8] mx-4"></div>
-              <div className=" font-inter text-xs leading-[16px] text-[#757B80] flex">
-                <span>Refill.&nbsp; </span>
-                <DropDown
-                  btnLabel={<span className="text-[#4167AF]">1</span>}
-                  endSlot={<Icon name="arrowDown" size={16} className="font-semibold text-[#4167AF]" />}
-                  items={refillOptions}
-                  className="p-1 h-[1rem]  font-inter font-normal text-[0.75rem] leading-4 text-[#757B80] border border-none"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-2 my-4">
-            <Badge icon="warning" level="Moderate" label="Previously prescribed" />
-          </div>
-          <div className="mb-5 bg-white border border-[#BFCFDA] rounded-[16px] box-border p-4">
-            <div className="font-medium font-inter text-sm leading-[20px] text-[#757B80]">Directions for use</div>
-            <div className="break-words whitespace-normal font-inter text-sm leading-[20px] text-[#000005]">
-              {medication.use}
-            </div>
-          </div>
-          <div className="mx-1">
-            <form className="my-5" onSubmit={handleSubmit}>
-              <input
-                type="text"
-                placeholder="Reason for compounding"
-                className="w-full h-[56px] p-5 bg-white border border-[#BFCFDA] rounded-[1rem] font-inter text-base leading-6 text-[#757B80] focus:outline-none focus:ring-2 focus:ring-blue-500"
-                value={reason}
-                onChange={(e) => setReason(e.target.value)}
+      <form className="" onSubmit={handleSubmit}>
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => {
+            onClose();
+          }}
+          title="Add to Order"
+        >
+          <div className="overflow-y-auto [&::-webkit-scrollbar]:none">
+            <div className="m-1">
+              <SelectDropDown
+                label="Select Patient"
+                options={patientData}
+                value={selectedPatient}
+                onChange={(value) => setSelectedPatient(value)}
               />
-            </form>
+            </div>
+            {physician.first_name && (
+              <Badge
+                label={`Prescribed by: ${physician.first_name}`}
+                className="mx-1 my-2 rounded-md bg-[#E6E9EA] font-inter font-medium text-[12px] leading-[16px] text-[#757B80]"
+              />
+            )}
+            <div className="flex flex-col justify-center mt-6 p-4 h-[52px] rounded-[12px] bg-[#F6F9FA]">
+              <div className="flex justify-between">
+                <span
+                  ref={textRef}
+                  className="p-1 font-inter font-medium text-sm leading-[20px] text-[#000005] flex-nowrap"
+                >
+                  {medication.name}
+                </span>
+                <div>
+                  <Icon name="paper1" onClick={copyToClipboard} />
+                </div>
+              </div>
+              <div className="flex">
+                <div className=" font-inter text-xs leading-[16px] text-[#757B80] flex">
+                  <span>Vial: &nbsp;</span>
+                  <DropDown
+                    btnLabel={<span className="text-[#4167AF]">{medication.vial}</span>}
+                    endSlot={<Icon name="arrowDown" size={16} className="font-semibold text-[#4167AF]" />}
+                    items={vialOptions}
+                    className="p-1 h-[1rem]  font-inter font-normal text-[0.75rem] leading-4 text-[#757B80] border border-none"
+                  />
+                </div>
+                <div className="border border-[#E6F0F8] mx-4"></div>
+                <div className=" font-inter text-xs leading-[16px] text-[#757B80] flex">
+                  <span>Qty.&nbsp;</span>
+                  <QuantityDropdown onSelect={(value) => setMedicationQuantity(value)} />
+                </div>
+                <div className="border border-[#E6F0F8] mx-4"></div>
+                <div className=" font-inter text-xs leading-[16px] text-[#757B80] flex">
+                  <span>Refill.&nbsp; </span>
+                  <DropDown
+                    btnLabel={<span className="text-[#4167AF]">1</span>}
+                    endSlot={<Icon name="arrowDown" size={16} className="font-semibold text-[#4167AF]" />}
+                    items={refillOptions}
+                    className="p-1 h-[1rem]  font-inter font-normal text-[0.75rem] leading-4 text-[#757B80] border border-none"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-2 my-4">
+              <Badge icon="warning" level="Moderate" label="Previously prescribed" />
+            </div>
+            <div className="mb-5 bg-white border border-[#BFCFDA] rounded-[16px] box-border p-4">
+              <div className="font-medium font-inter text-sm leading-[20px] text-[#757B80]">Directions for use</div>
+              <div className="break-words whitespace-normal font-inter text-sm leading-[20px] text-[#000005]">
+                {medication.use}
+              </div>
+            </div>
+            <div className="mx-1">
+              <form className="my-5" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  placeholder="Reason for compounding"
+                  className="w-full h-[56px] p-5 bg-white border border-[#BFCFDA] rounded-[1rem] font-inter text-base leading-6 text-[#757B80] focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  value={reason}
+                  onChange={(e) => setReason(e.target.value)}
+                />
+              </form>
+            </div>
+            <hr className=" border-t border-[#E6F0F8]" />
+            <div className="mx-1 mt-5 h-[56px]">
+              <SelectDropDown
+                label="Pharmacy"
+                options={pharmacyData}
+                value={selectedPharmacy}
+                onChange={(value, extraData) => {
+                  setSelectedPharmacy(value);
+                  if (extraData?.street_address && extraData?.city && extraData?.state && extraData?.zip)
+                    setPharmacyAddress(
+                      `${extraData?.street_address}, ${extraData?.city}, ${extraData?.state}, ${extraData?.zip}` || ''
+                    );
+                }}
+              />
+            </div>
+            {pharmacyAddress && (
+              <Badge
+                label={pharmacyAddress}
+                className="mt-4 mx-1 gap-2 rounded-md bg-[#E6E9EA] font-inter font-medium text-[12px] leading-[16px] text-[#757B80] pt-[3px] px-2 pb-[2px]"
+              />
+            )}
+            <hr className="mt-10 top-[678px] border-0 border-[#E6F0F8] border-t" />
+            <Button
+              variant="primary"
+              className="flex w-full my-5 items-center justify-center h-[3.5rem] bg-[#4167AF] rounded-[1rem]"
+              type="submit"
+            >
+              Place Order
+            </Button>
           </div>
-          <hr className=" border-t border-[#E6F0F8]" />
-          <div className="mx-1 mt-5 h-[56px]">
-            <SelectDropDown
-              label="Pharmacy"
-              options={pharmacyData}
-              value={selectedPharmacy}
-              onChange={(value, extraData) => {
-                setSelectedPharmacy(value);
-                if (extraData?.street_address && extraData?.city && extraData?.state && extraData?.zip)
-                  setPharmacyAddress(
-                    `${extraData?.street_address}, ${extraData?.city}, ${extraData?.state}, ${extraData?.zip}` || ''
-                  );
-              }}
-            />
-          </div>
-          {pharmacyAddress && (
-            <Badge
-              label={pharmacyAddress}
-              className="mt-4 mx-1 gap-2 rounded-md bg-[#E6E9EA] font-inter font-medium text-[12px] leading-[16px] text-[#757B80] pt-[3px] px-2 pb-[2px]"
-            />
-          )}
-          <hr className="mt-10 top-[678px] border-0 border-[#E6F0F8] border-t" />
-          <Button
-            variant="primary"
-            className="flex w-full my-5 items-center justify-center h-[3.5rem] bg-[#4167AF] rounded-[1rem]"
-            type="submit"
-          >
-            Place Order
-          </Button>
-        </div>
-      </Modal>
+        </Modal>
+      </form>
     </>
   );
 }
