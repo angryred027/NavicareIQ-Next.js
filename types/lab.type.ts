4b608b1beabefabfa00f51fe4d7fa8435c420c25
Angry Red <angryred027@gmail.com>
2025-03-23 03:52:05 -0500

    Create the type of lab data.

---
+++ types/lab.type.ts
@@ -0,0 +1,20 @@
+export type Lab = {
+  id: number;
+  name: string;
+  description?: string;
+  price: number;
+  type?: string;
+  code?: string;
+  lab_tests?: LabTest[];
+  created_at?: string;
+  updated_at?: string;
+};
+
+export type LabTest = {
+  id: number;
+  code?: string;
+  description?: string;
+  name: string;
+  created_at?: string;
+  updated_at?: string;
+};
