d4c838d34db69d8f69a77aa0685fcc46bd64e85f
Angry Red <angryred027@gmail.com>
2025-03-23 17:38:23 -0500

    Create a Redux slice for order data and store the data in local storage.

--- store/features/orderSlice.ts
+++ store/features/orderSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import { MedicationOrderItem, MedicationOrderItems } from '@/types/medication.type';
 import { LabOrderItem, LabOrderItems } from '@/types/lab.type';
 
@@ -12,28 +12,82 @@ const initialState: OrderState = {
   medicationOrders: [],
 };
 
+export const loadStoredOrderData = createAsyncThunk('order/loadStoredOrderData', async (_, { rejectWithValue }) => {
+  try {
+    const serializedState = localStorage.getItem('orderState');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return rejectWithValue('Failed to load localStorage.');
+  }
+});
+
+export const saveOrderStateToLocalStorage = (state: OrderState) => {
+  try {
+    localStorage.setItem('orderState', JSON.stringify(state));
+  } catch (error) {
+    console.error('Failed to save order state:', error);
+  }
+};
+
 const orderSlice = createSlice({
   name: 'order',
   initialState,
   reducers: {
     addLabOrder: (state, action: PayloadAction<LabOrderItem>) => {
       state.labOrders.push(action.payload);
+      saveOrderStateToLocalStorage(state);
     },
 
     addMedicationOrder: (state, action: PayloadAction<MedicationOrderItem>) => {
-      state.medicationOrders.push(action.payload);
+      const previousOrders = state.medicationOrders;
+      const payload = action.payload;
+      const index = previousOrders.findIndex((order) => order.medication?.id === payload.medication?.id);
+
+      if (index !== -1) {
+        state.medicationOrders[index].quantity += payload.quantity;
+      } else state.medicationOrders.push(action.payload);
+      console.log('Payload: ', action.payload);
+      saveOrderStateToLocalStorage(state);
     },
 
     setMedicationQuantity: (state, action: PayloadAction<MedicationOrderItem>) => {
-      let pos = -1;
-      if ((pos = state.medicationOrders.indexOf(action.payload))) {
-        state.medicationOrders[pos] = action.payload;
-      } else {
-        addMedicationOrder(action.payload);
+      const previousOrders = state.medicationOrders;
+      const payload = action.payload;
+      const index = previousOrders.findIndex((order) => order.medication?.id === payload.medication?.id);
+
+      if (index !== -1) {
+        state.medicationOrders[index].quantity = payload.quantity;
       }
+      saveOrderStateToLocalStorage(state);
     },
+
+    clearMedicationOrder: (state) => {
+      state.medicationOrders = [];
+      saveOrderStateToLocalStorage(state);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadStoredOrderData.pending, (state) => {
+        state.labOrders = [];
+        state.medicationOrders = [];
+      })
+      .addCase(loadStoredOrderData.fulfilled, (state, action) => {
+        if (action.payload) {
+          state.labOrders = action.payload.labOrders;
+          state.medicationOrders = action.payload.medicationOrders;
+        } else {
+          state.labOrders = [];
+          state.medicationOrders = [];
+        }
+      })
+      .addCase(loadStoredOrderData.rejected, (state, action) => {
+        state.labOrders = [];
+        state.medicationOrders = [];
+      });
   },
 });
 
-export const { addLabOrder, addMedicationOrder } = orderSlice.actions;
+export const { addLabOrder, addMedicationOrder, setMedicationQuantity, clearMedicationOrder } = orderSlice.actions;
 export default orderSlice.reducer;
--- store/store.ts
+++ store/store.ts
@@ -1,3 +1,4 @@
+// store.tsx
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
 import pageReducer from './features/pageSlice';
